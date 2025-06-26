88aeba7fb93054370cd7e5caab0321bb331d88e9
poga83 <66672062+poga83@users.noreply.github.com>
2025-06-26 12:07:13 +0300

    Update ConnectionManager.js

--- js/modules/ConnectionManager.js
+++ js/modules/ConnectionManager.js
@@ -1,12 +1,15 @@
 /* js/modules/ConnectionManager.js */
 import { CONFIG, pingServer } from '../config.js';
+
 export class ConnectionManager {
-  constructor(){ this.socket=null; }
-  async connect(){
+  constructor() { this.socket = null; }
+
+  async connect() {
     if (!await pingServer()) throw new Error('Сервер недоступен');
     this.socket = io(CONFIG.SERVER_URL, CONFIG.SOCKET);
-    this.socket.on('connect',()=> window.ui.notify('Подключено','success'));
-    this.socket.on('connect_error',e=> window.ui.notify('Нет соединения','error'));
+    this.socket.on('connect', () => window.ui.notify('Подключено к серверу', 'success'));
+    this.socket.on('connect_error', e => window.ui.notify('Нет соединения с сервером', 'error'));
   }
 }
+
 window.ConnectionManager = ConnectionManager;

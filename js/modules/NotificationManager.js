3965598b9846252c9d5a8d9483538fb8dcaab052
poga83 <66672062+poga83@users.noreply.github.com>
2025-06-26 12:06:38 +0300

    Update NotificationManager.js

--- js/modules/NotificationManager.js
+++ js/modules/NotificationManager.js
@@ -1,5 +1,5 @@
 /* js/modules/NotificationManager.js */
 export class NotificationManager {
-  show (msg,type='info') { window.ui.notify(msg,type); }
+  show(msg, type = 'info') { window.ui.notify(msg, type); }
 }
 window.NotificationManager = NotificationManager;
